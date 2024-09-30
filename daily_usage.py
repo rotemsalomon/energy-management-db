7c22942065ca89f118a8dfc234876d849af3dc19
rotemsalomon <rotem@raasify.io>
2024-09-30 21:37:47 +1000

    verify changes

--- daily_usage.py
+++ daily_usage.py
@@ -354,14 +354,15 @@ def process_metrics_for_hour(conn, cursor, daily_asset_records, current_hour, cu
 
             if response_time.date() == current_date and current_hour == response_time.hour: # If the date and hour in the response_time field of the record being processed = the current_date and current_hour value
                 asset_data[asset_id]['current_hour_kwh'] += kwh # Add kwh to usage
-                logging.info(f"Current hour kWh for {asset_id}: {asset_data[asset_id]['current_hour_kwh']}")
                 if asset_id not in first_response_time_current_hour:
                     first_response_time_current_hour[asset_id] = response_time  # Reset first response time
                 else:
                     asset_data[asset_id]['response_time_count'] += 1  # Count the records used for current_hour_kwh
                     #logging.info(f"Asset ID: {asset_id}, First Response Time for current hour: {first_response_time_current_hour[asset_id]}, Last Response Time for current hour: {last_response_time_current_hour[asset_id]}, Response Time Count: {asset_data[asset_id]['response_time_count']}")
                     last_response_time_current_hour[asset_id] = response_time
-
+            
+            logging.info(f"Current hour kWh for {asset_id}: {asset_data[asset_id]['current_hour_kwh']}")
+            
             # Cumulative kWh usage for this asset.
             asset_data[asset_id]['total_kwh'] += kwh # Add kwh (above) to the current asset_id total_kwh value.
             # Cumulative kwh for all assets.
