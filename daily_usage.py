200b02f0fe39655ca3e1b8ec0991b47c4f012162
rotemsalomon <rotem@raasify.io>
2024-10-01 20:05:06 +1000

    cummulating kwh per hour

--- daily_usage.py
+++ daily_usage.py
@@ -416,7 +416,7 @@ def process_metrics_for_hour(conn, cursor, daily_asset_records, current_hour, cu
             asset_data[asset_id]['last_hour'] = current_hour
 
             # Log or store the total_kwh and daily_total_kwh as needed
-            logging.info(f"Asset ID {asset_id} - Hour {current_hour}: Total kWh = {asset_data[asset_id]['total_kwh']}, Daily Total kWh = {asset_data[asset_id]['daily_total_kwh']}")
+            #logging.info(f"Asset ID {asset_id} - Hour {current_hour}: Total kWh = {asset_data[asset_id]['total_kwh']}, Daily Total kWh = {asset_data[asset_id]['daily_total_kwh']}")
 
             # Detect compressor ON transition
             if previous_power[asset_id] < 100 and power >= 100:
