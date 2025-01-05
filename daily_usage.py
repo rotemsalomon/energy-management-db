6a6399b0c102f33ca50307710397b18d2486610f
rotemsalomon <rotem@raasify.io>
2025-01-05 17:46:03 +1100

    daily metrics

--- daily_usage.py
+++ daily_usage.py
@@ -614,9 +614,9 @@ def process_metrics_for_hour(conn, cursor, daily_asset_records, current_hour, cu
                     percentage_change_kwh, current_hour_kwh, total_kwh_co2e, 
                     current_hour_kwh_co2e, day_of_week,
                     total_kwh_delta, total_kwh_charge_delta, total_kwh_co2e_delta,
-                    total_kwh_delta_percent, total_kwh_charge_delta_percent, total_kwh_co2e_delta_percent,
+                    total_kwh_delta_percent, total_kwh_charge_delta_percent, total_kwh_co2e_delta_percent
                 )
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 ON CONFLICT(asset_id, date, hour) DO UPDATE SET
                     org_id = excluded.org_id,
                     premise_id = excluded.premise_id,
@@ -638,7 +638,7 @@ def process_metrics_for_hour(conn, cursor, daily_asset_records, current_hour, cu
                     total_kwh_co2e_delta = excluded.total_kwh_co2e_delta,
                     total_kwh_delta_percent = excluded.total_kwh_delta_percent,
                     total_kwh_co2e_delta_percent = excluded.total_kwh_co2e_delta_percent,
-                    total_kwh_charge_delta_percent = excluded.total_kwh_charge_delta_percent,
+                    total_kwh_charge_delta_percent = excluded.total_kwh_charge_delta_percent
             ''', (
                 asset_id, org_id, premise_id, asset_name, current_date, 
                 round(total_kwh, 2), cnt_comp_on, cnt_comp_off, 
