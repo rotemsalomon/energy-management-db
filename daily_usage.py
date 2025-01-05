b502af0633492eb9f19c51f64da6e4d96ca02987
rotemsalomon <rotem@raasify.io>
2025-01-05 21:57:06 +1100

    daily metrics

--- daily_usage.py
+++ daily_usage.py
@@ -486,9 +486,7 @@ def process_metrics_for_hour(conn, cursor, daily_asset_records, current_hour, cu
                 #logging.info(f"No previous record found for asset {asset_id} on date {current_date} before hour {current_hour}")
 
             # Accumulate the current hour kWh to total_kwh and daily_total_kwh
-            total_kwh = previous_total_kwh + asset_data[asset_id]['current_hour_kwh']
-            daily_total_kwh = previous_daily_total_kwh + asset_data[asset_id]['current_hour_kwh']
- 
+            total_kwh = previous_total_kwh + asset_data[asset_id]['current_hour_kwh'] 
             # Update the last hour this asset was updated to the current hour
             asset_data[asset_id]['last_hour'] = current_hour
             #logging.info(f"The last hour = current hour: {current_hour} for {asset_id}")
