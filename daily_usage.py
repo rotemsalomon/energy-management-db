3eebb2a6307d71f8118ea811734cf3de8ebfee32
rotemsalomon <rotem@raasify.io>
2025-01-07 22:44:53 +1100

    specific_dates

--- daily_usage.py
+++ daily_usage.py
@@ -299,7 +299,7 @@ def compare_daily_with_benchmark(cursor, current_data):
     }
 #def get_missing_hours(cursor, static_date=None, static_hour=None):
 ## To specify date and hour to run script, comment above and update static_date, static_hour and uncomment below.
-def get_missing_hours(cursor, static_date="2025-01-01", static_hour="23"):
+def get_missing_hours(cursor, static_date="2024-12-31", static_hour="23"):
     try:
         # Use static values if provided, otherwise calculate dynamically
         if static_date is not None and static_hour is not None:
