d10c591cf595aa89ce4f21e41ace63c1302f9567
rotemsalomon <rotem@raasify.io>
2025-01-08 17:27:34 +1100

    logging

--- test_yaml.py
+++ test_yaml.py
@@ -1,6 +1,6 @@
 import yaml
 
-# Check if yaml is available
+# Check if yaml is available.
 try:
     yaml_version = yaml.__version__
     print(f"yaml module is installed, version: {yaml_version}")
