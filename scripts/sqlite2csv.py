169eb9e6654cfa28dcf00de234c436a3b1494b0d
rotemsalomon <rotem@raasify.io>
2025-01-09 15:31:15 +1100

    sqlite2csv

--- scripts/sqlite2csv.py
+++ scripts/sqlite2csv.py
@@ -2,35 +2,32 @@ import sqlite3
 import csv
 
 def table2csv(db_file, table_name, csv_name):
-    # Connect to the SQLite database
-    conn = sqlite3.connect(db_file)
-    cursor = conn.cursor()
-
     try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+
         # Query to select all data from the table
         cursor.execute(f'''
             SELECT * FROM {table_name} LIMIT 1;
         ''')
-        
-        print("Fetching all rows from the table {table_name}")
+
         # Fetch all rows
         rows = cursor.fetchall()
 
-        # Opening CSV and appending to file
-        print("Opening CSV and appending to file {csv_name}")
-
-        with open({csv_name}, 'w', newline='') as csvfile:
+        # Writing to CSV
+        with open(csv_name + '.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(rows)
 
         # Close the connection
-        print("CSV file writing to {csv_name} completed successfully")
         conn.close()
-        print("Connection to {db_file} successfully closed")
+        print(f"CSV file '{csv_name}.csv' has been successfully created.")
 
     except sqlite3.OperationalError as e:
         print(f"An error occurred: {e}")
-        return
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == '__main__':
     # Database file path and table name
